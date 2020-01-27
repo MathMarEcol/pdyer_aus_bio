b65e1f6808de55a4c19f19b1f83de6728ec2984e
Phil Dyer <phildyer@protonmail.com>
2020-01-27 17:01:39 +1000

    Add bind points for aus_bio singularity module

--- code/aus_bio_module.lua
+++ code/aus_bio_module.lua
@@ -7,6 +7,9 @@ whatis("philip.dyer1@uqconnect.edu.au")
 whatis("20200122 ")
 whatis("For more detail, run ")
 whatis(" ")
+
+setenv("SINGULARITY_BIND", "$TMPDIR:$TMPDIR")
+
 load("singularity/3.5.0")
 -- Run Rscript on stdin
 set_alias("Rscript","/sw/Containers/singularity/bin/run_singularity exec /30days/uqpdyer/Q1216/pdyer/pdyer_aus_bio/code/8kzygmabzzpzys35d728dri80r89rhws-docker-image-r-singularity-aus_bio_singularity_conversion.sif Rscript")
