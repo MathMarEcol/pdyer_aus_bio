ff73dfbe395acf7271f0d5ae5ab3f9f56cf6e5f2
Phil Dyer <phildyer@protonmail.com>
2020-03-02 13:32:37 +1000

    Copy module file every time, and be explicit about directory roots

--- code/aus_bio_pbs_submit.sh
+++ code/aus_bio_pbs_submit.sh
@@ -4,6 +4,7 @@
 #PBS -l walltime=00:10:00
 
 
+
 #by default, PBS begins in the home dir, but the env var $PBS_O_WORKDIR contains the path to this script.
 #Assuming that this job was called from /???30days???/uqpdyer/Q1216/pdyer/pdyer_aus_bio/code
 cd $PBS_O_WORKDIR
@@ -14,13 +15,13 @@ mkdir -p $TMPDIR/Q1216
 
 #Have to carefully make sure all files make it over
 mkdir -p $TMPDIR/Q1215/BioORACLE
-rsync -irc ../../../../Q1215/BioORACLE $TMPDIR/Q1215/BioORACLE
+rsync -irc $PBS_O_WORKDIR/../../../../Q1215/BioORACLE $TMPDIR/Q1215/BioORACLE
 mkdir -p $TMPDIR/Q1215/AusCPR
-rsync -irc ../../../../Q1215/AusCPR/combined_copeped_jul19.csv $TMPDIR/Q1215/AusCPR
+rsync -irc $PBS_O_WORKDIR/../../../../Q1215/AusCPR/combined_copeped_jul19.csv $TMPDIR/Q1215/AusCPR
 mkdir -p $TMPDIR/Q1215/ShapeFiles/World_EEZ_v8
-rsync -irc ../../../../Q1215/ShapeFiles/World_EEZ_v8 $TMPDIR/Q1215/ShapeFiles/World_EEZ_v8
+rsync -irc $PBS_O_WORKDIR/../../../../Q1215/ShapeFiles/World_EEZ_v8 $TMPDIR/Q1215/ShapeFiles/World_EEZ_v8
 mkdir -p $TMPDIR/Q1216/pdyer/pdyer_aus_bio/code
-rsync -irc ./drake_plan.R $TMPDIR/Q1216/pdyer/pdyer_aus_bio/code
+rsync -irc $PBS_O_WORKDIR/drake_plan.R $TMPDIR/Q1216/pdyer/pdyer_aus_bio/code
 
 #Set up the output directory
 mkdir -p $TMPDIR/Q1216/pdyer/pdyer_aus_bio/outputs
@@ -32,10 +33,13 @@ cd $TMPDIR/Q1216/pdyer/pdyer_aus_bio/code
 #-s means set, expand_aliases is only default for interactive shells, not non-interactive
 shopt -s expand_aliases
 
+#Always use the latest module, so I don't need to remember to copy it manually
+cp $PBS_O_WORKDIR/aus_bio_module.lua ~/privatemodules/aus_bio_module.lua
+
 module load use.own
 module load aus_bio_module
 #Because this is a singularity, it can only see directories specified in aus_bio_module's "SINGULARITY_BIND" env var.
-Rscript drake_plan.R
+Rscript $TMPDIR/Q1216/pdyer/pdyer_aus_bio/code/drake_plan.R
 
 
 #Recover the outputs
