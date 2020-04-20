fced25d8d7dc43d1cb306336da2b736b4688d571
Phil Dyer <phildyer@protonmail.com>
2020-04-20 13:54:46 +1000

    Add expand_aliases to lua module

--- code/aus_bio_module.lua
+++ code/aus_bio_module.lua
@@ -8,13 +8,14 @@ whatis("20200122 ")
 whatis("For more detail, run ")
 whatis(" ")
 
-setenv("SINGULARITY_BIND", os.getenv("TMPDIR") .. ",/opt,/sw7,/sw,/sw6,/gpfs1/scratch/30days:/30days,/gpfs1/homes:/home") 
-setenv("SINGULARITYENV_PREPEND_PATH", "/opt/pbs/bin")
+--setenv("SINGULARITY_BIND", os.getenv("TMPDIR") .. ",/opt,/sw7,/sw,/sw6,/gpfs1/scratch/30days:/30days,/gpfs1/homes:/home") 
+--setenv("SINGULARITYENV_PREPEND_PATH", "/opt/pbs/bin")
 
 load("singularity/3.5.0")
 singularity = "/sw/Containers/singularity/bin/run_singularity"
 aus_bio_sif = "/home/uqpdyer/aus_bio_test.sif"
 -- Run Rscript on stdin
+execute{cmd="shopt -s expand_aliases", modeA={"all"}}
 set_alias("Rscript", singularity .. " exec " .. aus_bio_sif ..  " Rscript")
 set_alias("R", singularity .. " exec " .. aus_bio_sif .. " R --vanilla" )
 set_alias("drake_build", singularity .. " exec " .. aus_bio_sif .. " Rscript --vanilla" )
