8faff4dc87a1c7110739448a051555079cd88eec
Phil Dyer <phildyer@protonmail.com>
2020-04-22 15:35:12 +1000

    Fix rsync archive to RDM

--- code/rdm_up.sh
+++ code/rdm_up.sh
@@ -8,4 +8,4 @@
 rsync -irc /90days/uqpdyer/rdm_mirror/Q1215/bioORACLE /QRISdata/Q1215/
 
 tar --xz -cf  /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio
-rsync -irc /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz /QRISdata/Q1215/pdyer/pdyer_aus_bio.tar.xz
+rsync -irc /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz /QRISdata/Q1215/pdyer/
