bf2f715311fd0fda8684ab9cb7eea4b2fa863121
Phil Dyer <phildyer@protonmail.com>
2020-04-22 15:18:10 +1000

    Add decompress location for tarball

--- code/rdm_down.sh
+++ code/rdm_down.sh
@@ -5,4 +5,4 @@ rsync -irc  /QRISdata/Q1215/AusCPR /90days/uqpdyer/rdm_mirror/Q1215
 rsync -irc  /QRISdata/Q1215/ShapeFiles/World_EEZ_v8 /90days/uqpdyer/rdm_mirror/Q1215/ShapeFiles
 
 rsync -irc /QRISdata/Q1215/pdyer/pdyer_aus_bio.tar.xz /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz
-tar --xz -xf  /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio
+tar --xz -xf  /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz -C /90days/uqpdyer/rdm_mirror/Q1216/pdyer
