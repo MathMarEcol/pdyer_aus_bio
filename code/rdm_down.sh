80d7712edff9462b078a8c85dc228ff2dd19766e
Phil Dyer <phildyer@protonmail.com>
2020-04-22 15:13:45 +1000

    Store projects as tar.xz archives

--- code/rdm_down.sh
+++ code/rdm_down.sh
@@ -4,4 +4,5 @@ rsync -irc  /QRISdata/Q1215/bioORACLE /90days/uqpdyer/rdm_mirror/Q1215
 rsync -irc  /QRISdata/Q1215/AusCPR /90days/uqpdyer/rdm_mirror/Q1215
 rsync -irc  /QRISdata/Q1215/ShapeFiles/World_EEZ_v8 /90days/uqpdyer/rdm_mirror/Q1215/ShapeFiles
 
-rsync -irc  /QRISdata/Q1216/pdyer/pdyer_aus_bio /90days/uqpdyer/rdm_mirror/Q1216/pdyer/
+rsync -irc /QRISdata/Q1215/pdyer/pdyer_aus_bio.tar.xz /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz
+tar --xz -xf  /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio
--- code/rdm_up.sh
+++ code/rdm_up.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+#All processing is done in 90days
+#RDM is for storage.
+
+
+
 rsync -irc /90days/uqpdyer/rdm_mirror/Q1215/bioORACLE /QRISdata/Q1215/
-rsync -irc /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio /QRISdata/Q1215/pdyer
+
+tar --xz -cf  /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio
+rsync -irc /90days/uqpdyer/rdm_mirror/Q1216/pdyer/pdyer_aus_bio.tar.xz /QRISdata/Q1215/pdyer/pdyer_aus_bio.tar.xz
