57487984df42ff590ce899e1d558635d459c8eac
Phil Dyer <phildyer@protonmail.com>
2021-03-30 16:54:22 +1000

    Improve species name cleaning

--- code/drake_plan.R
+++ code/drake_plan.R
@@ -49,6 +49,7 @@ length_pre <- length(unique(x))
 x %>%
     stringr::str_replace_all("\\(.*\\)", "") %>%
     stringr::str_replace_all(" ", "_") %>%
+    stringr::str_replace_all("-", "_") %>%
     stringr::str_replace_all("__", "_") -> y
 length_post <- length(unique(y))
 assertthat::assert_that(length_pre == length_post)
