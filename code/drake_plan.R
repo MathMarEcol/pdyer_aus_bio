cf40dc8e2ea0ccdf736262235dbbc5d7d8823de3
Phil Dyer <phildyer@protonmail.com>
2021-04-28 10:01:25 +1000

    Add another case for cleaning names

--- code/drake_plan.R
+++ code/drake_plan.R
@@ -51,6 +51,7 @@ x %>%
     stringr::str_replace_all("\\(.*\\)", "") %>%
     stringr::str_replace_all(" ", "_") %>%
     stringr::str_replace_all("-", "_") %>%
+    stringr::str_replace_all("/", "") %>%
     stringr::str_replace_all("__", "_") -> y
 length_post <- length(unique(y))
 assertthat::assert_that(length_pre == length_post)
