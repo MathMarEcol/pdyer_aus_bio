77e9155cb00ccb2cda068193fe7f26c6765c0842
Phil Dyer <phildyer@protonmail.com>
2021-05-05 11:14:27 +1000

    Update nixpkgs version

--- code/nixpkgs_rev.nix
+++ code/nixpkgs_rev.nix
@@ -1,16 +1,12 @@
 import (builtins.fetchGit {
   # Descriptive name to make the store path easier to identify
-  name = "nixos-unstable-2020-05-01";
+  name = "nixos-unstable-2021-05-04";
   # url = https://github.com/nixos/nixpkgs-channels/;
   url = https://github.com/PhDyellow/nixpkgs/;
   # Commit hash for nixos-unstable as of 2020-01-20
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  ref = "refs/heads/master_plus_patches_3"; #using bleeding edge packages
-  rev = "a10b00e080415531e09eb4c9ec6b195f54a50625";
-
-
-
-
+  ref = "refs/heads/master"; #using bleeding edge packages
+  rev = "37e2fbda397880a38af91dbf266b19cd3955ed07";
 
 })
     { #the attributes to import
