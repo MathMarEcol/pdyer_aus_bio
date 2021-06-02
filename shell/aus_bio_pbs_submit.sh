ce4ca41e58459d791822b52b2e8bc81ed37aa1c0
Phil Dyer <phildyer@protonmail.com>
2021-06-02 11:43:47 +1000

    master job needs very little resources

--- shell/aus_bio_pbs_submit.sh
+++ shell/aus_bio_pbs_submit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #PBS -A UQ-SCI-SMP
-#PBS -l select=1:ncpus=2:mem=60GB
-#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=1:mem=3GB
+#PBS -l walltime=167:00:00
 #PBS -q Single
 #
 #
--- shell/aus_bio_slurm_submit.sh
+++ shell/aus_bio_slurm_submit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH ntasks=1
-#SBATCH cpus-per-task=2
-#SBATCH --mem-per-cpu=20GB
-#SBATCH --time=0-6:00
+#SBATCH cpus-per-task=1
+#SBATCH --mem-per-cpu=4GB
+#SBATCH --time=7-00:00
 #SBATCH -o slurm.%N.%j.%a.out  # STDOUT
 #SBATCH -e slurm.%N.%j.%a.err  # STDERR
 
