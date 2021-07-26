27e0558aeef19a851524eef3e8e250caf0ea6f0b
Phil Dyer <phildyer@protonmail.com>
2021-07-26 12:21:29 +1000

    Better merging of biological samples
    Estimate number of temporally separated
    samples at each survey location (site_id).
    
    Take the average (or agg_fun) across all
    samples, which will be >= the number of
    sites, and assuming 0 if no abundance
    is recorded in the long form data.

--- R/functions/merge_bio_env.R
+++ R/functions/merge_bio_env.R
@@ -56,8 +56,46 @@ merge_bio_env <- function(
 
   ## Merge sites into grid cells
 
-  obs_env <- obs_env[, .(abund = agg_fun(abund)), by = c("taxon_id", "env_id")]
-  sites_env <- sites_env[, .(site_id = list(c(site_id))), by = c("env_id")]
+  ## Need to know how many samples total, not just non-zeros
+  site_times_est <- obs[, .(n_times = .N), by = c("site_id", "taxon_id")][,
+                                                                          .(n_times = max(n_times)), by = site_id]
+  grid_times_est <- purrr::map2_df(sites_env$site_id, sites_env[[env_id_col]], ~{
+    out <- sum(site_times_est[site_id %in% .x, n_times])
+    ret <- data.table(n_total = out, .y)
+    names(ret) <- c("n_total", env_id_col)
+    return(ret)
+    })
+  sites_env <- sites_env[, .(site_id = list(c(site_id)), n_sites = length(c(site_id))), by = env_id_col]
+  sites_env[grid_times_est , n_total := i.n_total, on = env_id_col]
+
+  ## For each env_id and taxon combination, find the merged abundance
+  ## Also need to merge across time.
+  ## Don't have time factor, need to estimate
+  grid_taxa_cross <- data.table::CJ(sites_env[[env_id_col]], taxa$taxon_id)
+  names(grid_taxa_cross) <- c(env_id_col, "taxon_id")
+  grid_taxa_cross[sites_env, c("n_sites", "n_total")  := .(i.n_sites, i.n_total), on = env_id_col]
+  grid_env <- grid_taxa_cross[,
+                  {
+                    n_total <- .SD$n_total
+                    if(nrow(obs_env[taxon_id == .BY$taxon_id &
+                            obs_env[[env_id_col]] == .BY[[env_id_col]]]) == 0) {
+                      ret <- 0
+                    } else {
+                      ret <- obs_env[taxon_id == .BY$taxon_id &
+                                     obs_env[[env_id_col]] == .BY[[env_id_col]],
+                      {
+                        .(abund = agg_fun(
+                            c(abund,
+                              rep(0, n_total - length(abund))
+                              )
+                          ))
+                      }
+                      ]
+                    }
+                  },
+                  by = c(env_id_col, "taxon_id")]
+
+  names(grid_env)[3] <- "abund"
 
 
   ## Filter species by
@@ -66,33 +104,34 @@ merge_bio_env <- function(
   ## min_occurrences
   n_sites <-nrow(sites_env)
 
-taxa_keep <- obs_env[ ,
-        .(keep =
-  .N >= min_occurrence & # within survey and depth, taxon occurs in more than min_occurrences locations
-   sd(c(abund, rep(0, n_sites - .N))) / mean(c(abund, rep(0, n_sites - .N))) >= cov_min &
-  .N/n_sites >= min(freq_range) &
-  .N/n_sites <= max(freq_range),
-  cov = sd(c(abund, rep(0, n_sites - .N))) / mean(c(abund, rep(0, n_sites - .N))),
-  freq = .N/n_sites,
-  min_occ = .N
-        ),
-    by = c("taxon_id")]
+  taxa_keep <- grid_env[ ,
+                        .(cov = sd(abund) / mean(abund),
+                          freq = sum(abund != 0)/.N,
+                          occ = sum(abund != 0)
+                          ),
+                        by = c("taxon_id")]
 
-taxa_keep <- taxa_keep[keep == TRUE]
+  taxa_keep[, keep :=
+                occ >= min_occurrence & # within survey and depth, taxon occurs in more than min_occurrences locations
+                cov >= cov_min &
+                freq >= min(freq_range) &
+                freq <= max(freq_range)]
 
-## if the number of taxa is still huge, keep the top
-## max_taxa with the best coefficient of variation
+  taxa_keep <- taxa_keep[keep == TRUE]
+
+  ## if the number of taxa is still huge, keep the top
+  ## max_taxa with the best coefficient of variation
   if (nrow(taxa_keep) > max_taxa) {
       taxa_keep <- head(taxa_keep[order(-cov)], n = max_taxa)
   }
 
-obs_env <- obs_env[taxon_id %in% taxa_keep$taxon_id]
+  grid_env <- grid_env[taxon_id %in% taxa_keep$taxon_id]
 
 
 ## Species data and env data are ready to be merged
 
-taxa[, taxon_id_chr := paste0("sp.",taxon_id)]
-obs_env[, taxon_id_chr := paste0("sp.",taxon_id)]
+  taxa[, taxon_id_chr := paste0("sp.",taxon_id)]
+  grid_env[, taxon_id_chr := paste0("sp.",taxon_id)]
 
 wide_surv <- env_domain[get(env_id_col) %in% sites_env[[env_id_col]]]
 ## wide_surv <- rbind(wide_surv, env_domain[1,])
@@ -106,7 +145,7 @@ wide_taxa_env[, (unique(obs_env$taxon_id_chr)) :=
  return(data.table(all_bio_long[,.(trophic, survey, depth_cat)],
                    wide_taxa_env = list(wide_taxa_env),
                    taxa = list(taxa),
-                   obs_env = list(obs_env),
+                   obs_env = list(grid_env),
                    sites_env = list(sites_env)
                    ))
 }
