fb35a6d62be56d1474f1b7ba612f72ed3321d941
Phil Dyer <phildyer@protonmail.com>
2021-08-19 17:07:24 +1000

    Add missing param

--- R/_targets.R
+++ R/_targets.R
@@ -291,6 +291,7 @@ list(
       env_domain, ## this target knows it's own env_domain provenance, and loads in the appropriate env_domain branch.
       env_biooracle_names,
       extrap,
+      pred_importance_top,
       env_id_col
     ),
     pattern = map(gfbootstrap_combined)
