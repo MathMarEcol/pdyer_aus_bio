a800240c152e84ba6ce6cc8b5208a6727c5875b0
Phil Dyer <phildyer@protonmail.com>
2021-09-22 16:21:53 +1000

    By default, use workers to load and save data

--- R/_targets.R
+++ R/_targets.R
@@ -19,6 +19,15 @@ configure_parallel(default_clustermq = TRUE,
 tar_option_set(
   ##qs format is smaller and reads/writes faster than RDS. needs qs package.
   format = "qs",
+  ## By default, don't load anything into the master
+  ## The workers have access to the same FS, and
+  ## can read from the cache just as easily as
+  ## master, but then we don't have to store
+  ## all the info in memory twice.
+  storage = "worker",
+  retrieval = "worker",
+  garbage_collection = TRUE,
+  memory = "transient",
   ## Load libraries here. Faster than library(..) at top of this file.
   packages = c(
     "gfbootstrap",
