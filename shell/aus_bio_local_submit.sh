de37d24a218ccfdb181698c6231eab663fe90aa1
Phil Dyer <phildyer@protonmail.com>
2023-02-24 14:21:01 +1000

    change default filesystem roots

--- shell/aus_bio_local_submit.sh
+++ shell/aus_bio_local_submit.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 #W use "GIT_BRANCH=tagname WORKERS=2 ./aus_bio_pbs_local.sh"
-export ROOT_STORE_DIR="/vmshare/PARA/resources/qris_sandbox" #directory with same structure as /QRISdata/. May even be /QRISdata, but probably shouldn't be
-export TMPDIR_SHARE="/vmshare/PARA/resources/hpc_sandbox"
+export ROOT_STORE_DIR="/para/resources/qris_sandbox" #directory with same structure as /QRISdata/. May even be /QRISdata, but probably shouldn't be
+export TMPDIR_SHARE="/para/resources/hpc_sandbox"
 export COPY_MODULES=0 #don't copy HPC module files locally
 export GIT_BRANCH=${GIT_BRANCH:-"develop"} #to submit a particular tag
 export WORKERS=${WORKERS:-2} # 2 workers by default,
