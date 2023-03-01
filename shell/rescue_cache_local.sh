94500fb983a33e336da1280fb7a50fceff1a8a6c
Phil Dyer <phildyer@protonmail.com>
2023-03-01 13:23:39 +1000

    Change default paths in rescue_cache_local.sh

--- shell/rescue_cache_local.sh
+++ shell/rescue_cache_local.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 
-export ROOT_STORE_DIR="/vmshare/PARA/resources/qris_sandbox" #directory with same structure as /QRISdata/. May even be /QRISdata, but probably shouldn't be
-export TMPDIR_SHARE=${1:-"/vmshare/PARA/resources/hpc_sandbox"}
+export ROOT_STORE_DIR="/para/resources/qris_sandbox" #directory with same structure as /QRISdata/. May even be /QRISdata, but probably shouldn't be
+export TMPDIR_SHARE=${1:-"/para/resources/hpc_sandbox"}
 echo "Rescuing cache and outputs from local run"
 source ./rescue_cache_content.sh
