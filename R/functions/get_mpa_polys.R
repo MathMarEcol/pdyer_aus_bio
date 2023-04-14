81817330a7f15b19a67214cf42bf64eac901e1da
Phil Dyer <phildyer@protonmail.com>
2023-04-14 15:58:35 +1000

    Speed up mpa polygon generation

--- R/functions/get_mpa_polys.R
+++ R/functions/get_mpa_polys.R
@@ -6,7 +6,7 @@ get_mpa_polys <- function(
   ## This line is sensitive to internet quality in wdpar version 1.3.1.3
   country_mpa <- wdpar::wdpa_fetch(country_code, wait = TRUE)
   ## slow, definitely worth spinning out into a separate target
-  country_mpa_clean <- wdpar::wdpa_clean(country_mpa)
+  country_mpa_clean <- wdpar::wdpa_clean(country_mpa, erase_overlaps = FALSE)
   data.table::setDT(country_mpa_clean)
   country_mpa_clean_sub <- country_mpa_clean[IUCN_CAT %in% iucn_categories,] ## could also look at NO_TAKE
   country_mpa_clean_sub <- country_mpa_clean_sub[MARINE %in% marine_categories,]
