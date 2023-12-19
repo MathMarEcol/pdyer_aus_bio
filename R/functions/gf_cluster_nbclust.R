e066d6412ebe40960731aca4a2c5f270196068ba
Phil Dyer <phildyer@protonmail.com>
2023-12-19 14:25:27 +1000

    Handle NbClust errors gracefully
    return the error and `best_nc = 0`.

--- R/functions/gf_cluster_nbclust.R
+++ R/functions/gf_cluster_nbclust.R
@@ -17,22 +17,38 @@ fit_nbclust <- function(
                       ))
   }
 
-  gf_nbclust <- NbClust::NbClust(
-    gf_predicted$comp_turnover[[1]][, env_biooracle_names, with = FALSE],
-    distance = nbclust_dist,
-    min.nc = min(k_range),
-    max.nc = max(k_range),
-    method = nbclust_method,
-    index = nbclust_index
+
+  gf_nbclust <- tryCatch(
+    {
+      NbClust::NbClust(
+        gf_predicted$comp_turnover[[1]][, env_biooracle_names, with = FALSE],
+        distance = nbclust_dist,
+        min.nc = min(k_range),
+        max.nc = max(k_range),
+        method = nbclust_method,
+        index = nbclust_index
+      )
+    },
+    error = function(e) {
+      return(e)
+    }
   )
+
+  if (inherits(gf_nbclust, "error")) {
+    best_nc <- 0
+  } else {
+    best_nc <- gf_nbclust$Best.nc["Number_clusters"]
+  }
 
   return(data.table(gf_predicted[, .(env_domain, trophic, survey, depth_cat)],
                     clust_method = "kmedoids",
                     gf_nbclust = list(gf_nbclust),
-                    best_nc = gf_nbclust$Best.nc["Number_clusters"],
+                    best_nc = best_nc,
                     nbclust_index = nbclust_index
                     ))
 }
+
+
 
 merge_nbclust <- function(
                           gf_cluster_nbclust_tmp
