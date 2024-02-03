7c4602e9e9464744be53b1dcd66da89c2ff8f6b9
Phil Dyer <phildyer@protonmail.com>
2024-02-03 18:24:12 +1000

    Prevent output files overwriting eachother

--- shell/aus_bio_prime_ai_batch.sh
+++ shell/aus_bio_prime_ai_batch.sh
@@ -7,8 +7,8 @@
 #SBATCH --time=168:00:00
 #SBATCH --partition=cpu
 #SBATCH --account=$1
-#SBATCH -o aus_bio_output
-#SBATCH -e aus_bio_error
+#SBATCH -o aus_bio_output_%j
+#SBATCH -e aus_bio_error_%j
 
 
 ## Contains job submission parameters for control job
