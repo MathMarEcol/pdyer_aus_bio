16c7c79e2fe9f74c2b3e157a32c314a484ae9805
Phil Dyer <phildyer@protonmail.com>
2024-02-05 18:03:01 +1000

    Use sbatch by loading in from container host

--- R/_targets.R
+++ R/_targets.R
@@ -43,23 +43,28 @@ ccg <- switch(host_trunc,
                     seconds_wall = 24 * 60 * 60, # 1 day
                     reset_globals = FALSE,
                     garbage_collection = TRUE,
-                    command_submit = if(Sys.getenv("SLURM_SUBMIT_HOST") == "") {
-                                        ## No Slurm Host, not in a slurm job
-                                        as.character(Sys.which("sbatch"))
-                                    } else {
-                                        ## In a slurm job, probably in a container
-                                        "ssh -i ~/.ssh/selfkey ${SLURM_SUBMIT_HOST:-'no_host_not_in_a_slurm_job'} sbatch"
-                                    },
-                    command_terminate = if(Sys.getenv("SLURM_SUBMIT_HOST") == "") {
+
+                    ## This is not working
+                    ## command_submit = "ssh -i ~/.ssh/selfkey prime-ai-nixos sbatch",
+                    ## command_submit = if(Sys.getenv("SLURM_SUBMIT_HOST") == "") {
+                    ##                     ## No Slurm Host, not in a slurm job
+                    ##                     as.character(Sys.which("sbatch"))
+                    ##                 } else {
+                    ##                     ## In a slurm job, probably in a container
+                    ##                     "ssh -i ~/.ssh/selfkey ${SLURM_SUBMIT_HOST:-'no_host_not_in_a_slurm_job'} sbatch"
+                    ##                 },
+                    ## command_terminate = if(Sys.getenv("SLURM_SUBMIT_HOST") == "") {
                                          ## No Slurm Host, not in a slurm job
-                                         as.character(Sys.which("scancel"))
-                                     } else {
+                                         ## as.character(Sys.which("scancel"))
+                                     ## } else {
                                          ## In a slurm job, probably in a container
-                                         "ssh -i ~/.ssh/selfkey ${SLURM_SUBMIT_HOST:-'no_host_not_in_a_slurm_job'} scancel"
-                                     },
-                    script_lines = "alias R apptainer exec ${APPTAINER_SIF_RUN} R", # May need to tune for apptainer, perhaps by creating a bash alias to Rscript
-                    slurm_log_output = file.path(Sys.getenv("LOGDIR"), "crew_log_%A.txt"),
-                    slurm_log_error = file.path(Sys.getenv("LOGDIR"), "crew_log_error_%A.txt"),
+                                         ## "ssh -i ~/.ssh/selfkey ${SLURM_SUBMIT_HOST:-'no_host_not_in_a_slurm_job'} scancel"
+                                     ## },
+
+                    ## This is sufficient to make runnable slurm workers
+                    script_lines = paste0("alias R='apptainer exec ", Sys.getenv("APPTAINER_SIF_RUN"), " R'"), # May need to tune for apptainer, perhaps by creating a bash alias to Rscript
+                    slurm_log_output = file.path(Sys.getenv("LOGDIR", "."), "crew_log_%A.txt"),
+                    slurm_log_error = file.path(Sys.getenv("LOGDIR", "."), "crew_log_error_%A.txt"),
                     slurm_memory_gigabytes_per_cpu = 4,
                     slurm_cpus_per_task = 1,
                     slurm_time_minutes = 24 * 60,
@@ -67,6 +72,7 @@ ccg <- switch(host_trunc,
                   )
                 )
 })
+
 
 
 
