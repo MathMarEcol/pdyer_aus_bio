eb4edeb1380994c3a1a57a293403027395472dec
Phil Dyer <phildyer@protonmail.com>
2024-02-16 18:48:56 +1000

    Adjust shell script

--- shell/setup_nix_shell_batch.sh
+++ shell/setup_nix_shell_batch.sh
@@ -7,6 +7,6 @@
 #SBATCH -o setup_output_%j
 #SBATCH -e setup_error_%j
 
-
+echo $SLURM_CPUS_PER_TASK
 
 srun nix build --no-link --max-jobs $SLURM_CPUS_PER_TASK --print-out-paths github:PhDyellow/nix_r_dev_shell/6bc2bb1f068e9fe6289be59671d3e7183bf4a142#devShells.x86_64-linux.r-shell
