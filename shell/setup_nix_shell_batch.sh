e07fd648acfea366a2c5194c2dab3850eb5b103e
Phil Dyer <phildyer@protonmail.com>
2024-02-17 12:46:45 +1000

    Memory was limiting factor

--- shell/setup_nix_shell_batch.sh
+++ shell/setup_nix_shell_batch.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=40G
+#SBATCH --mem=55G
 #SBATCH --job-name=aus_bio
 #SBATCH --time=12:00:00
 #SBATCH -o setup_output_%j
 #SBATCH -e setup_error_%j
 
 env
-srun nix build --no-link --cores $SLURM_CPUS_PER_TASK --max-jobs 1 --print-out-paths github:PhDyellow/nix_r_dev_shell/6bc2bb1f068e9fe6289be59671d3e7183bf4a142#devShells.x86_64-linux.r-shell
+srun nix build --no-link --cores $SLURM_CPUS_PER_TASK --max-jobs $SLURM_CPUS_PER_TASK --print-out-paths github:PhDyellow/nix_r_dev_shell/6bc2bb1f068e9fe6289be59671d3e7183bf4a142#devShells.x86_64-linux.r-shell
