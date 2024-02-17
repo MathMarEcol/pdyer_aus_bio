1720155eab6594e0977df115a6bae0ca90e6d389
Phil Dyer <phildyer@protonmail.com>
2024-02-17 12:33:54 +1000

    Don't over-sell CPUs during nix shell setup

--- shell/setup_nix_shell_batch.sh
+++ shell/setup_nix_shell_batch.sh
@@ -8,4 +8,4 @@
 #SBATCH -e setup_error_%j
 
 env
-srun nix build --no-link --cores $SLURM_CPUS_PER_TASK --max-jobs $SLURM_CPUS_PER_TASK --print-out-paths github:PhDyellow/nix_r_dev_shell/6bc2bb1f068e9fe6289be59671d3e7183bf4a142#devShells.x86_64-linux.r-shell
+srun nix build --no-link --cores $SLURM_CPUS_PER_TASK --max-jobs 1 --print-out-paths github:PhDyellow/nix_r_dev_shell/6bc2bb1f068e9fe6289be59671d3e7183bf4a142#devShells.x86_64-linux.r-shell
